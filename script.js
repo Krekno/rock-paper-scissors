e2238a854849120bab4a10f217a8dad2a3e3c765
Krekno <yigiterenkarabas@gmail.com>
2024-01-31 21:58:48 +0300

    app can take user input now and fixed a syntax error

--- script.js
+++ script.js
@@ -4,7 +4,22 @@
 2: scissors
 */
 
-function getComputerChoice {
+function getComputerChoice() {
     Math.floor(Math.random(3));
 }
 
+function getPlayerChoice() {
+    let playerChoice = prompt("Rock, Paper, or Scissors?");
+    for (let i = 0; i < playerChoice.length; i++) {
+        playerChoice[i] = playerChoice[i].toLowerCase();
+    }
+    if (playerChoice == "rock") {
+        return 0;
+    } else if (playerChoice == "paper") {
+        return 1;
+    } else if (playerChoice == "scissors") {
+        return 2;
+    } else {
+        return "Invalid input. Please try again.";
+    }
+}
